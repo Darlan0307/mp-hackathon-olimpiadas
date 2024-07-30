ef6491366d2a820b19a8981fb5c86f8dd2badec2
Darlan0307 <darlanchagas2020@gmail.com>
2024-07-30 12:43:30 -0300

    feat: componentes do dashboard

---
+++ src/components/aside-component.tsx
@@ -0,0 +1,50 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Flag, Home, Package2 } from "lucide-react";
+import Link from "next/link";
+
+const AsideComponent = () => {
+  return (
+    <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+      <nav className="flex flex-col items-center gap-4 px-2 sm:py-4">
+        <Link
+          href="/"
+          className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+        >
+          <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
+          <span className="sr-only">Acme Inc</span>
+        </Link>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link
+              href="/"
+              className="bg-accenttext-accent-foreground flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+            >
+              <Home className="h-5 w-5" />
+              <span className="sr-only">Dashboard</span>
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent side="right">Dashboard</TooltipContent>
+        </Tooltip>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link
+              href="/countries"
+              className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+            >
+              <Flag className="h-5 w-5" />
+
+              <span className="sr-only">Countries</span>
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent side="right">Countries</TooltipContent>
+        </Tooltip>
+      </nav>
+    </aside>
+  );
+};
+
+export default AsideComponent;
---
+++ src/components/header-component.tsx
@@ -0,0 +1,59 @@
+import Link from "next/link";
+import { Flag, Home, Package2, PanelLeft } from "lucide-react";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "./mode-toggle";
+
+const HeaderComponent = () => {
+  return (
+    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button size="icon" variant="outline" className="sm:hidden">
+            <PanelLeft className="h-5 w-5" />
+            <span className="sr-only">Toggle Menu</span>
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="sm:max-w-xs">
+          <SheetTitle className="mb-6">
+            <Link
+              href="/"
+              className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
+            >
+              <Package2 className="h-5 w-5 transition-all group-hover:scale-110" />
+              <span className="sr-only">Acme Inc</span>
+            </Link>
+          </SheetTitle>
+          <nav className="grid gap-6 text-lg font-medium">
+            <Link
+              href="/"
+              className="flex items-center gap-4 px-2.5 text-foreground hover:text-foreground"
+            >
+              <Home className="h-5 w-5" />
+              Dashboard
+            </Link>
+            <Link
+              href="/countries"
+              className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+            >
+              <Flag className="h-5 w-5" />
+              Countries
+            </Link>
+          </nav>
+        </SheetContent>
+      </Sheet>
+
+      <div className="ml-auto flex-1 grow-0">
+        <ModeToggle />
+      </div>
+    </header>
+  );
+};
+
+export default HeaderComponent;
