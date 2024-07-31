53bfbbd0d7f41357f5aae736bb80ced23b127b6f
Darlan0307 <darlanchagas2020@gmail.com>
2024-07-31 12:43:36 -0300

    fix: ajustando logica das estilizações do aside e do menu do hearder

--- src/components/aside-component.tsx
+++ src/components/aside-component.tsx
@@ -1,3 +1,4 @@
+"use client";
 import {
   Tooltip,
   TooltipContent,
@@ -5,43 +6,52 @@ import {
 } from "@/components/ui/tooltip";
 import { Flag, Home, SquareDashedBottomCode } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const AsideComponent = () => {
+  const path = usePathname();
+
+  const Links = [
+    {
+      href: "/",
+      icon: <Home className="h-5 w-5" />,
+      label: "Dashboard",
+    },
+    {
+      href: "/countries",
+      icon: <Flag className="h-5 w-5" />,
+      label: "Countries",
+    },
+  ];
+
   return (
     <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
       <nav className="flex flex-col items-center gap-4 px-2 sm:py-4">
         <Link
           href="/"
-          className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base mb-6"
+          className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg  font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base mb-6"
         >
           <SquareDashedBottomCode className="h-6 w-6 transition-all group-hover:scale-110" />
           <span className="sr-only">Acme Inc</span>
         </Link>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Link
-              href="/"
-              className="bg-accenttext-accent-foreground flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-            >
-              <Home className="h-5 w-5" />
-              <span className="sr-only">Dashboard</span>
-            </Link>
-          </TooltipTrigger>
-          <TooltipContent side="right">Dashboard</TooltipContent>
-        </Tooltip>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Link
-              href="/countries"
-              className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-            >
-              <Flag className="h-5 w-5" />
-
-              <span className="sr-only">Countries</span>
-            </Link>
-          </TooltipTrigger>
-          <TooltipContent side="right">Countries</TooltipContent>
-        </Tooltip>
+        {Links.map((link) => (
+          <Tooltip key={link.href}>
+            <TooltipTrigger asChild>
+              <Link
+                href={link.href}
+                className={`flex h-9 w-9 items-center justify-center rounded-lg   transition-colors  hover:text-foreground md:h-8 md:w-8 ${
+                  path == link.href
+                    ? "bg-accent text-foreground"
+                    : "text-muted-foreground"
+                }`}
+              >
+                {link.icon}
+                <span className="sr-only">{link.label}</span>
+              </Link>
+            </TooltipTrigger>
+            <TooltipContent side="right">{link.label}</TooltipContent>
+          </Tooltip>
+        ))}
       </nav>
     </aside>
   );
--- src/components/header-component.tsx
+++ src/components/header-component.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import { Flag, Home, PanelLeft, SquareDashedBottomCode } from "lucide-react";
 
@@ -9,45 +10,71 @@ import {
 } from "@/components/ui/sheet";
 import { Button } from "@/components/ui/button";
 import { ModeToggle } from "./mode-toggle";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const HeaderComponent = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const path = usePathname();
+
+  const Links = [
+    {
+      href: "/",
+      icon: <Home className="h-5 w-5" />,
+      label: "Dashboard",
+    },
+    {
+      href: "/countries",
+      icon: <Flag className="h-5 w-5" />,
+      label: "Countries",
+    },
+  ];
   return (
     <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
-      <Sheet>
-        <SheetTrigger asChild>
-          <Button size="icon" variant="outline" className="sm:hidden">
-            <PanelLeft className="h-5 w-5" />
-            <span className="sr-only">Toggle Menu</span>
-          </Button>
-        </SheetTrigger>
+      <Button
+        size="icon"
+        variant="outline"
+        className="sm:hidden"
+        onClick={() => setIsOpen(true)}
+      >
+        <PanelLeft className="h-5 w-5" />
+        <span className="sr-only">Toggle Menu</span>
+      </Button>
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
         <SheetContent side="left" className="sm:max-w-xs">
           <SheetTitle className="mb-6">
             <Link
               href="/"
               className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
+              onClick={() => setIsOpen(false)}
             >
               <SquareDashedBottomCode className="h-8 w-8 transition-all group-hover:scale-110" />
               <span className="sr-only">Acme Inc</span>
             </Link>
           </SheetTitle>
           <nav className="grid gap-6 text-lg font-medium">
-            <Link
-              href="/"
-              className="flex items-center gap-4 px-2.5 text-foreground hover:text-foreground"
-            >
-              <Home className="h-5 w-5" />
-              Dashboard
-            </Link>
-            <Link
-              href="/countries"
-              className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
-            >
-              <Flag className="h-5 w-5" />
-              Countries
-            </Link>
+            {Links.map((link) => (
+              <Link
+                key={link.href}
+                href={link.href}
+                onClick={() => setIsOpen(false)}
+                className={`flex items-center gap-4 px-2.5 text-foreground hover:text-foreground ${
+                  path == link.href
+                    ? "text-foreground"
+                    : "text-muted-foreground"
+                }`}
+              >
+                {link.icon}
+                {link.label}
+              </Link>
+            ))}
           </nav>
         </SheetContent>
       </Sheet>
+
+      <h1 className="text-center flex-1 sm:text-2xl font-bold">
+        Olympic Games 2024
+      </h1>
 
       <div className="ml-auto flex-1 grow-0">
         <ModeToggle />
